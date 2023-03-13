a6a3a17c723103ffefa5de379529fdc011088723
openaphid <openaphid@gmail.com>
2023-03-13 10:29:10 +0800

    remove deprecated source code

--- copy_release_tmp.sh
+++
@@ -1,8 +0,0 @@
-set -e
-set -x
-
-mkdir -p /tmp/searchgram-release
-
-fd -I -e apk . TMessagesProj/build/outputs/apk/afat/release/ | xargs -I {} cp {} /tmp/searchgram-release/
-cp TMessagesProj/build/outputs/mapping/afatRelease/mapping.txt /tmp/searchgram-release/
-cp TMessagesProj/build/outputs/native-debug-symbols/afatRelease/native-debug-symbols.zip /tmp/searchgram-release/
